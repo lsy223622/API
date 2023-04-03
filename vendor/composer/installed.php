f3f50e7799f3400614613d477cdad2a504bfb11b
lsy223622 <lsy223622@outlook.com>
2023-04-03 15:06:11 +0800

    Add Sabre/VObject

---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'sabre/uri' => 
+    array (
+      'pretty_version' => '3.0.0',
+      'version' => '3.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0ab547f34cf15a735e976a285ffc0cfcc27a3f82',
+    ),
+    'sabre/vobject' => 
+    array (
+      'pretty_version' => '4.5.3',
+      'version' => '4.5.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fe6d9183154ed6f2f913f2b568d3d51d8ae9b308',
+    ),
+    'sabre/xml' => 
+    array (
+      'pretty_version' => '4.0.0',
+      'version' => '4.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5051b30416b60f52395007c7069acbd010edd2a7',
+    ),
+  ),
+);
