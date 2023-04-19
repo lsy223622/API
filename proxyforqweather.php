166c11829957f8d340b434b362cff11a6e5ff402
lsy223622 <lsy223622@outlook.com>
2023-04-19 12:03:25 +0800

    Create proxyforqweather.php

---
+++ proxyforqweather.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'credentials.php';
+
+$cache_time = 3 * 60; // 缓存有效时间，单位为秒
+$cache_file = 'data/cached_qweather.json'; // 本地保存的JSON文件名
+
+// 检查缓存文件是否存在且在有效期内
+if (file_exists($cache_file) && time() - filemtime($cache_file) < $cache_time) {
+    // 返回本地缓存的JSON文件内容
+    $json_data = file_get_contents($cache_file);
+} else {
+    // 请求远程JSON文件并保存到本地
+    $json_data = file_get_contents('compress.zlib://'.qWeatherAPI);
+    file_put_contents($cache_file, $json_data);
+}
+
+// 返回JSON数据并记录访问时间
+echo $json_data;
